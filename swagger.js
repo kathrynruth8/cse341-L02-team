821c8c2c8cf2ec71fde16b7e995ab462dc4931eb
Kathryn Lambert <113531714+kathrynruth8@users.noreply.github.com>
2025-05-17 21:17:05 -0600

    fix url for render to work

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'Contacts API',
   },
   host: 'cse341-l02-ec83.onrender.com',
-  schemes: ['http'],
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
